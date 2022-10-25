f62b3d0688c3947675e9fe8101cc89aab8e1f9ed
Sylvestre Gug <sylvestre@observablehq.com>
2022-10-25 13:14:46 -0400

    cleanup

--- data/init_mssql.sql
+++
@@ -1,5 +0,0 @@
-RESTORE DATABASE test
-    FROM DISK = '/var/opt/mssql/backup/test.bak'
-    WITH REPLACE, RECOVERY,
-    MOVE 'AdventureWorksLT2012_Data' TO '/var/opt/mssql/data/aw2019.mdf',
-    MOVE 'AdventureWorksLT2012_Log'  TO '/var/opt/mssql/data/aw2019.ldf';
