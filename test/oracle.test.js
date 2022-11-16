c706c11190867c8a5088098e0dbc6daf5b879f14
Sylvestre Gug <sylvestre@observablehq.com>
2022-11-16 11:25:09 -0500

    sql query stream pipe

--- test/oracle.test.js
+++ test/oracle.test.js
@@ -32,10 +32,12 @@ describe("oracle", () => {
   });
 
   describe("when querying", () => {
-    it("should stream the results of simple query", () => {
+    it("should stream the results of a simple query", () => {
       return new Promise(async (resolve, reject) => {
         const req = new MockReq({method: "POST", url: "/query-stream"}).end({
-          sql: "SELECT 1 AS A_1 FROM nodetab",
+          sql: `SELECT 1 AS A_1, 
+                'A_REGULAR_STRING' AS A_2
+                FROM DUAL`,
           params: [],
         });
 
@@ -52,11 +54,74 @@ describe("oracle", () => {
               type: "array",
               items: {
                 type: "object",
-                properties: {CustomerID: {type: ["null", "integer"]}},
+                properties: {
+                  A_1: {
+                    type: ["null", "number"],
+                  },
+                  A_2: {
+                    type: ["null", "string"],
+                  },
+                },
               },
             })
           );
-          expect(row).to.equal(JSON.stringify({CustomerID: 12}));
+
+          expect(row).to.equal(
+            JSON.stringify({A_1: 1, A_2: "A_REGULAR_STRING"})
+          );
+
+          resolve();
+        }
+      });
+    });
+    it("should stream the results of a query including dates format", () => {
+      return new Promise(async (resolve, reject) => {
+        const req = new MockReq({method: "POST", url: "/query-stream"}).end({
+          sql: `SELECT TO_TIMESTAMP('2005-04-04 09:30:00', 'YYYY-MM-DD HH:MI:SS') AS A_SIMPLE_DATE,
+                 TO_DATE('January 15, 1989, 11:00 A.M.', 'Month dd, YYYY, HH:MI A.M.','NLS_DATE_LANGUAGE = American') AS A_TEST_DATE,
+                 TO_TIMESTAMP_TZ('1999-12-0111:00:00-08:00','YYYY-MM-DDHH:MI:SSTZH:TZM') AS A_TZ_TIMESTAMP,
+                 TO_DATE('January 15, 1989, 11:00 A.M.', 'Month dd, YYYY, HH:MI A.M.','NLS_DATE_LANGUAGE = American') + TO_DSINTERVAL('100 10:00:00') AS A_INTERVAL,
+                 TO_DATE('January 15, 1989, 11:00 A.M.', 'Month dd, YYYY, HH:MI A.M.','NLS_DATE_LANGUAGE = American') + TO_YMINTERVAL('01-02') AS A_INTERVAL_TIME_MINUTE
+                FROM DUAL`,
+          params: [],
+        });
+
+        const res = new MockRes(onEnd);
+
+        const index = oracle(credentials);
+        await index(req, res);
+
+        function onEnd() {
+          const [schema, row] = this._getString().split("\n");
+
+          expect(schema).to.equal(
+            JSON.stringify({
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  A_SIMPLE_DATE: {type: ["null", "string"], date: true},
+                  A_TEST_DATE: {type: ["null", "string"], date: true},
+                  A_TZ_TIMESTAMP: {type: ["null", "string"], date: true},
+                  A_INTERVAL: {type: ["null", "string"], date: true},
+                  A_INTERVAL_TIME_MINUTE: {
+                    type: ["null", "string"],
+                    date: true,
+                  },
+                },
+              },
+            })
+          );
+
+          expect(row).to.equal(
+            JSON.stringify({
+              A_SIMPLE_DATE: "2005-04-04T14:30:00.000Z",
+              A_TEST_DATE: "1989-01-15T16:00:00.000Z",
+              A_TZ_TIMESTAMP: "1999-12-01T19:00:00.000Z",
+              A_INTERVAL: "1989-04-26T02:00:00.000Z",
+              A_INTERVAL_TIME_MINUTE: "1990-03-15T16:00:00.000Z",
+            })
+          );
 
           resolve();
         }
