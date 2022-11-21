d6b984eb2183afe4805a06d503b5cb5a9398afc1
Sylvestre Gug <sylvestre@observablehq.com>
2022-11-21 10:53:42 -0500

    WIP

--- test/oracle.test.js
+++ test/oracle.test.js
@@ -8,22 +8,20 @@ import {ORACLE_CREDENTIALS} from "../.env.test.js";
 const credentials = ORACLE_CREDENTIALS;
 
 describe("oracle", async (done) => {
-  describe("when pooling", () => {});
-
   describe("when checking", () => {
-    describe("with system admin user", () => {
-      it.skip("should return ok", async (done) => {
+    describe("with a READ_ONLY user", () => {
+      it("should return ok", async () => {
         const req = new MockReq({
           method: "POST",
           url: "/check",
         });
         const res = new MockRes();
         const index = oracle(credentials);
-        done();
-        // const response = await index(req, res);
-        //
-        // expect(response.statusCode).to.equal(200);
-        // expect(response.body.ok).to.equal(true);
+
+        const response = await index(req, res);
+
+        expect(response.statusCode).to.equal(200);
+        expect(response.body.ok).to.equal(true);
       });
     });
   });
