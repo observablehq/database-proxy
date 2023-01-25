ad278933deddd9a9c92e672292862509eba74bbf
Sylvestre Gug <sylvestre@observablehq.com>
2023-01-25 16:45:39 -0500

    add checking test

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -1,7 +1,27 @@
 import assert from "node:assert";
+import MockReq from "mock-req";
+import MockRes from "mock-res";
+import logger from "../middleware/logger.js";
+import mysql from "../lib/mysql.js";
 
-describe("mssql", () => {
-  it("should test", () => {
-    assert.equal(1, 1);
+import {MYSQL_CREDENTIALS} from "../.env.test.js";
+const index = logger(mysql(MYSQL_CREDENTIALS));
+
+describe("MySQL", () => {
+  describe("when checking", () => {
+    it("should do MySQL credential check", () => {
+      const req = new MockReq({method: "POST", url: "/check"});
+      const res = new MockRes();
+
+      return index(req, res).then(
+        () => Promise.reject("Expect call to throw!"),
+        (err) => {
+          assert.equal(
+            /User has too permissive grants/.test(err.message),
+            true
+          );
+        }
+      );
+    });
   });
 });
